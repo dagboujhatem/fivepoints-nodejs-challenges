ae289561185f815e1df13750eecb97976290ec0e
dagboujhatem <dagboujhatem@gmail.com>
2021-02-18 17:04:14 +0100

    method two

--- passport/passport.js
+++ passport/passport.js
@@ -3,14 +3,26 @@ const BearerStrategy = require("passport-http-bearer").Strategy;
 const User = require("../models/userSchema");
 const jwt = require("jsonwebtoken");
 
-passport.use(
-  new BearerStrategy(async (token, done) => {
-    const tokenData = await jwt.verify(token, 'secret');
-    const user = await User.findOne({ _id: tokenData.userId });
-    if (!user) {
-      return done(null, false);
-    } else {
-      return done(null, user);
-    }
-  })
-);
+// passport.use(
+//   new BearerStrategy(async (token, done) => {
+//     const tokenData = await jwt.verify(token, 'secret');
+//     const user = await User.findOne({ _id: tokenData.userId });
+//     if (!user) {
+//       return done(null, false);
+//     } else {
+//       return done(null, user);
+//     }
+//   })
+// );
+
+
+passport.use(new BearerStrategy(async(token, done) => {
+   const decodedData = await jwt.verify(token, 'secret');
+   console.log(decodedData);
+    User.findOne({ _id: decodedData.userId }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user, { scope: 'all' });
+    });
+  }
+));
